139d7effe1047a5d41c6a7be7a6714d576937dd8
Hamza ESSAYEGH <hamza.essayegh@protonmail.com>
2018-03-06 12:05:54 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,39 @@
+"""
+Flask-Encryptor
+-------------
+
+This Flask extension attempts to ease users' uploaded file encryption.
+    - PyCrypto is used to encrypt/decrypt files
+    - Automatically rename and associate encrypted files
+    - Can be used to generate a download link and immediately delete the decrypted file
+"""
+from setuptools import setup
+
+
+setup(
+    name                    = 'Flask-Encryptor',
+    version                 = '1.0',
+    # url                     = 'http://example.com/flask-sqlite3/',
+    license                 = 'BSD',
+    author                  = 'Hamza ESSAYEGH',
+    author_email            = 'hamza.essayegh@protonmail.com',
+    description             = 'Flask extension helping encrypting users personal files',
+    long_description        = __doc__,
+    py_modules              = ['flask_encryptor'],
+    zip_safe                = False,
+    include_package_data    = True,
+    platforms               ='any',
+    install_requires=[
+        'flask',
+        'pycrypto'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
